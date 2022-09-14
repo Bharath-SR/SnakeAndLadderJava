b3e6d738c687454c550ff66c93fface505641303
Bharath-SR <bharath.s0941@gmail.com>
2022-09-14 20:29:44 +0530

    UC3: Added Changes to Snake And Ladder

--- java/SnakeAndLadder/SnakeAndLadder/src/com/bridgeLabz/SnakeAndLadder.java
+++ java/SnakeAndLadder/SnakeAndLadder/src/com/bridgeLabz/SnakeAndLadder.java
@@ -18,12 +18,32 @@ public class SnakeAndLadder {
 		//Driving Class
 		//Initializing and declaring final since there will be only one player
 		final int noOfPlayer=1;
-		int startpostion =0;
-		int n=0;
+		int position =0;
+		int diceno =0;
+		int positionmoved;
+		int checkForOption;
+		//  for dice
 		Random r = new Random();
-		n=r.nextInt(6)+1;
-
-		System.out.println(n);
+		diceno =r.nextInt(6)+1;
+		System.out.println("diceno"+diceno);
+		
+		// for option
+		Random r1 = new Random();
+		checkForOption = r1.nextInt(3);
+		System.out.println("option"+checkForOption);
+		if(checkForOption == 0) {
+			System.out.println("NO PLAY");
+			positionmoved = 0;
+			System.out.println("positionMoved"+positionmoved);
+		}else if(checkForOption == 1) {
+			System.out.println("LADDER");
+			positionmoved= position+diceno;
+			System.out.println("positionMoved"+positionmoved);
+		}else if(checkForOption == 2){
+			System.out.println("SNAKE");
+			positionmoved= position-diceno;
+			System.out.println("positionMoved"+positionmoved);
+		}
 	}
 
 }
