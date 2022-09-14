b832b52b60558c30ed158947a549e98073c18ad2
Bharath-SR <bharath.s0941@gmail.com>
2022-09-14 20:38:11 +0530

    Added:UC6 Snake And Ladder

--- java/SnakeAndLadder/SnakeAndLadder/src/com/bridgeLabz/SnakeAndLadder.java
+++ java/SnakeAndLadder/SnakeAndLadder/src/com/bridgeLabz/SnakeAndLadder.java
@@ -10,10 +10,9 @@ import java.util.Random;
  *
  */
 public class SnakeAndLadder {
-
 	/**
-	 * UC5
-	 * Ensure the player gets to exact winning position 100
+	 * UC6
+	 * Report the number of times the dice was played to win the game and also the position after every die role
 	 */
 	public static void main(String[] args) {
 		//Driving Class
@@ -21,21 +20,23 @@ public class SnakeAndLadder {
 		final int noOfPlayer=1;
 		int position =0;
 		int diceno =0;
+		int dicecount=0;
 		int positionmoved = 0;
 		int checkForOption;
 		while (positionmoved < 100) {
-		//  for dice
+			// Random Function for rolling dies
+			dicecount ++;
 		Random r = new Random();
 		diceno =r.nextInt(6)+1;
 		System.out.println("diceno"+diceno);
-		
-		// for option
+		System.out.println("no of dice roled"+dicecount+"\s"+"dice position"+diceno);	
+		//Random Function for rolling  option
 		Random r1 = new Random();
 		checkForOption = r1.nextInt(3);
 		System.out.println("option"+checkForOption);
 		if(checkForOption == 0) {
 			System.out.println("NO PLAY");
-			positionmoved = 0+positionmoved;
+			positionmoved = positionmoved;
 			System.out.println("positionMoved"+positionmoved);
 		}else if(checkForOption == 1) {
 			System.out.println("LADDER");
@@ -53,6 +54,7 @@ public class SnakeAndLadder {
 			System.out.println(positionmoved);
 			if(positionmoved > 100) {
 				positionmoved = positionmoved - diceno;
+				System.out.println("positionmoved after if"+positionmoved);
 			}
 		}
 	}
